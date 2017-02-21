fa0bf9b85a5e3cafb8589b3d4cb3cf365f33907f
Danny Harding <dannyharding10@gmail.com>
2017-02-20 18:59:50 -0700

    most of the html stuff

--- taskStructure.js
+++ taskStructure.js
@@ -1,25 +1,25 @@
 var tasks = {
-  'taskName': {
-  	name: 'taskName',
-	subTasks: {
-	  'subTaskName': {
-		name: 'subTaskName',
-		time: 5,
+	'taskName': {
+		name: 'taskName',
+		subtasks: {
+			'subtaskName': {
+				name: 'subtaskName',
+				time: 5,
+				complete: false
+			},
+			'subtask2Name': {
+				name: 'subtask2Name',
+				time: 10,
+				complete: false
+			}
+		},
+		time: 15,
 		complete: false
-	  },
-	  'subTask2Name': {
-		name: 'subTask2Name',
-		time: 10,
-		complete: false
-	  }
 	},
-	time: 15,
-	complete: false
-  },
-  'task2Name': {
-  	name: 'task2Name',
-  	subTasks: {},
-  	time: 0,
-  	complete: false
-  }
+	'task2Name': {
+		name: 'task2Name',
+		subtasks: {},
+		time: 0,
+		complete: false
+	}
 }
---
+++ todo.js
@@ -0,0 +1,57 @@
+var taskTemplate = '<div class="task">' +
+						'<taskInfo task="task">' +
+						'<div ng-repeat="subtask in task.subtasks" class="subtask-container">' +
+							'<taskInfo task="subtask">' +
+						'</div>' +
+					'</div>';
+
+var taskInfoTemplate = '<div class="task-info">' +
+							'<div class="task-icon"></div>' +
+							'<div class="task-title"></div>' +
+							'<div class="task-time"></div>' +
+							'<div class="task-completed-container">' +
+								'<input type="checkbox" >' +
+							'</div>' +
+						'</div>';
+
+angular.module('todo-app', [])
+  .controller('mainController', mainController)
+  .directive('task', taskDirective)
+  .directive('subtask', subtaskDirective);
+
+function mainController($scope) {
+	$scope.tasks = {
+		'taskName': {
+			name: 'taskName',
+			subtasks: {
+				'subtaskName': {
+					name: 'subtaskName',
+					time: 5,
+					complete: false
+				},
+				'subtask2Name': {
+					name: 'subtask2Name',
+					time: 10,
+					complete: false
+				}
+			},
+			time: 15,
+			complete: false
+		},
+		'task2Name': {
+			name: 'task2Name',
+			subtasks: {},
+			time: 0,
+			complete: false
+		}
+	};
+
+	$scope.addTask = function() {
+		// add task
+	}
+
+}
+
+function taskDirective() {
+
+}
